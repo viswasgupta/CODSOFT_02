81e2410139aa743c110abc09423d7dc29ce4be7a
viswasgupta <118326079+viswasgupta@users.noreply.github.com>
2024-06-19 07:14:04 +0530

    Add files via upload

---
+++ StudentGrades.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class StudentGrades {
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int numSubjects = 6;
+        int[] marks = new int[numSubjects];
+        
+        System.out.println("Enter marks for " + numSubjects + " subjects (out of 100):");
+        for (int i = 0; i < numSubjects; i++) {
+            System.out.print("Subject " + (i + 1) + ": ");
+            marks[i] = scanner.nextInt(); 
+        }
+        
+        int totalMarks = 0;
+        for (int mark : marks) {
+            totalMarks += mark;
+        }
+        
+        double averagePercentage = (double) totalMarks / numSubjects;
+        
+        char grade;
+        if (averagePercentage >= 90) {
+            grade = 'A';
+        } else if (averagePercentage >= 80) {
+            grade = 'B';
+        } else if (averagePercentage >= 70) {
+            grade = 'C';
+        } else if (averagePercentage >= 60) {
+            grade = 'D';
+        } else {
+            grade = 'F';
+        }
+        
+        System.out.println("\nResults:");
+        System.out.println("Total Marks: " + totalMarks);
+        System.out.printf("Average Percentage: %.2f%%\n", averagePercentage);
+        System.out.println("Grade: " + grade);
+        
+        scanner.close();
+    }
+}
